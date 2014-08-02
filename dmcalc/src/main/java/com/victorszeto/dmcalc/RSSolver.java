566238ee6114d9c101a6967ef93906468ef6dc57
Timothy Tong <timothy.tong@hotmail.ca>
2014-08-02 17:04:01 -0400

    Added getRemainderStrings method

--- dmcalc/src/main/java/com/victorszeto/dmcalc/RSSolver.java
+++ dmcalc/src/main/java/com/victorszeto/dmcalc/RSSolver.java
@@ -1,6 +1,7 @@
 package com.victorszeto.dmcalc;
 
 import java.math.BigInteger;
+import java.util.HashMap;
 
 /**
  * Created by timothytong on 2014-06-18.
@@ -14,8 +15,10 @@ public class RSSolver {
     private static BigInteger b;
     private static BigInteger m;
     private static int counter = 1;
-
+    private static HashMap<BigInteger,BigInteger> map;
+    private static ArrayList<String> remainderStrings = new ArrayList<String>();
     public static BigInteger repeatedSquare(BigInteger base, BigInteger exponent, BigInteger mod) {
+        map = new HashMap<BigInteger, BigInteger>();
         a= base;
         b = exponent;
         m = mod;
@@ -29,27 +32,49 @@ public class RSSolver {
         } else if (a.mod(m) == BigInteger.valueOf(1l)) {
             r = BigInteger.valueOf(1l);
         } else {
+            BigInteger lastRemainder = r;
+            map.put(a.mod(m), BigInteger.ONE);
             while (!expo.equals(b) ) {
-
                 if ((expo.multiply(BigInteger.valueOf(2l))).compareTo(b) == -1 || (expo.multiply(BigInteger.valueOf(2l))).compareTo(b) == 0) {
                     expo = expo.multiply(BigInteger.valueOf(2l));
                     r = (r.multiply(r)).mod(m);
-                   System.out.println("exp = " + expo + ", remainder = " + r);
+//                    System.out.println("exp = " + expo + ", remainder = " + r);
                     remainders.add(r);
+                    map.put(expo,r);
+//                    System.out.println(String.format("(key: %d,value: %d)",r,expo));
                     counter++;
+                    BigInteger previousRemainder;
+                    if(counter>2){
+                        previousRemainder = remainders.get(remainders.size() - 2);
+                    }
+                    else{
+                        previousRemainder = a;
+                    }
+                    System.out.println(String.format("%d^%d≡%d x %d≡ %d",a,expo,previousRemainder,previousRemainder,remainders.get(remainders.size()-1)));
+                    remainderStrings.add(String.format("%d^%d≡%d x %d≡ %d",a,expo,previousRemainder,previousRemainder,remainders.get(remainders.size()-1)));
                 } else {
+//                    System.out.println("PART 2");
                     for (int i = counter; i >= 0; i--) {
                         if (expo.add(BigInteger.valueOf((long) Math.pow(2, i))).compareTo(b) == -1 || expo.add(BigInteger.valueOf((long) Math.pow(2, i))).compareTo(b) == 0) {
                             expo = expo.add(BigInteger.valueOf((long)Math.pow(2, i)));
                             counter++;
                             r = (r.multiply(remainders.get(i+1))).mod(m);
-
-                            System.out.println("exp = " + expo + ", remainder = " + r);
+                            map.put(r,expo);
+                            remainders.add(r);
+                            BigInteger previousRemainder = remainders.get(remainders.size()-2);
+//                            System.out.println(String.format("fetching key-value pair key %d",BigInteger.valueOf((long)Math.pow(2,i))));
+                            remainderStrings.add(String.format("%d^%d≡%d x %d≡ %d",a,expo,previousRemainder,map.get(BigInteger.valueOf((long)Math.pow(2,i))),remainders.get(remainders.size()-1)));
+                            System.out.println(String.format("%d^%d≡%d x %d≡ %d",a,expo,previousRemainder,map.get(BigInteger.valueOf((long)Math.pow(2,i))),remainders.get(remainders.size()-1)));
+//                            System.out.println("exp = " + expo + ", remainder = " + r);
                         }
                     }
                 }
             }
         }
         return r;
+    }
+
+    public static ArrayList<String> getRemainderStrings(){
+        return remainderStrings;
     }
 }
